83a0d43a6605f7ca565c84a21da3394568950d8d
Rafassz <rafassz.aps@gmail.com>
2025-01-31 15:47:12 -0300

    Arrumando lupa de busca

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,7 +18,18 @@ export default function Navbar() {
 
     return (
         <nav className="flex justify-between items-center bg-gray-900 text-white p-4">
-            <div className="text-xl font-bold">Devlivery</div>
+            {/*Corrigir o link para home pela logo*/}
+            <img src="https://i.imgur.com/fBJcXWf.png" alt="LogoDevlivery" className="w-32"></img>
+
+            <div className="flex items-center gap-2 border rounded-full w-full md:w-1/3 lg:w-1/4 px-4 py-2 bg-gray-200">
+                <input
+                    type="text"
+                    placeholder="Buscar..."
+                    className="bg-transparent text-black outline-none w-full"
+                />
+                <Search size={24} className="text-gray-600" />
+            </div>
+
 
             <div className="relative" ref={menuRef}>
                 <button onClick={() => setMenuOpen(!menuOpen)} className="p-2 rounded-full bg-gray-700 hover:bg-gray-600">
