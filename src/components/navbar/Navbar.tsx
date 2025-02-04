4042aa15aa4cb4fb35dec616ae2426236336b59c
Rafassz <rafassz.aps@gmail.com>
2025-02-04 09:57:29 -0300

    Navbar com erro

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { ShoppingCart, User } from "lucide-react";
 import Search from "../search/Search";
 import { AuthContext } from "../../context/AuthContext";
 import { ToastAlerta } from "../../utils/ToastAlerta";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ModalPerfil from "../../pages/modalperfil/ModalPerfil";
 import Perfil from "../../pages/perfil/Perfil";
 
@@ -13,25 +13,23 @@ function Navbar() {
   const [open, setOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
-  const token = localStorage.getItem("token");
 
   const { usuario, handleLogout } = useContext(AuthContext);
+  const [token, setToken] = useState<string | null>(localStorage.getItem("token"));
 
   useEffect(() => {
+    setToken(localStorage.getItem("token"));
+
     function handleClickOutside(event: MouseEvent) {
       if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
         setMenuOpen(false);
       }
     }
+
     document.addEventListener("mousedown", handleClickOutside);
 
-    
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 50);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -40,13 +38,14 @@ function Navbar() {
       document.removeEventListener("mousedown", handleClickOutside);
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [usuario]);
 
   const handleCartClick = () => {
-    if (!token) {
-      alert("Você precisa estar logado para acessar o carrinho!");
+    if (!token || token === "undefined") {
+      ToastAlerta("Você precisa estar logado para acessar o carrinho!", "error");
+      navigate("/login");
     } else {
-      window.location.href = "/carrinho";
+      navigate("/carrinho");
     }
   };
 
@@ -56,27 +55,22 @@ function Navbar() {
     navigate("/home");
   }
 
-  let component: ReactNode;
-
   return (
     <nav
-      className={`flex justify-between items-center bg-gray-900 text-white p-4 ${
-        isScrolled ? "fixed top-0 left-0 w-full z-10" : ""
-      }`}
+      className={`flex justify-between items-center bg-gray-900 text-white p-4 ${isScrolled ? "fixed top-0 left-0 w-full z-10" : ""
+        }`}
     >
-      <img
-        src="https://i.imgur.com/fBJcXWf.png"
-        alt="LogoDevlivery"
-        className="w-16 ml-10"
-      />
+      <Link to="/home">
+        <img src="https://i.imgur.com/fBJcXWf.png" alt="LogoDevlivery" className="w-16 ml-10" />
+      </Link>
 
       <Search />
 
       <div className="flex items-center space-x-6 pr-4">
-        <a href=""><ShoppingCart
+        <ShoppingCart
           className="w-6 h-6 text-[#fff6cc] cursor-pointer hover:text-[#ff3c00]"
           onClick={handleCartClick}
-        /></a>
+        />
 
         <div className="relative" ref={menuRef}>
           <button
@@ -88,55 +82,48 @@ function Navbar() {
 
           {menuOpen && (
             <div className="absolute z-30 right-0 mt-2 w-48 bg-[#fff6cc] text-[#002914] rounded-lg shadow-lg overflow-hidden animate-fade-in">
-              {usuario.token !== ""
-                ? (component = (
-                    <>
-                      <a
-                        onClick={() => setOpen(true)}
-                        className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
-                      >
-                        <ModalPerfil open={open} onClose={() => setOpen(false)}>
-                          <Perfil />
-                        </ModalPerfil>
-                        Meu Perfil
-                      </a>
-                      <a
-                        href="/carrinho"
-                        className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
-                      >
-                        Meu Carrinho
-                      </a>
-                      <a
-                        href="/about"
-                        className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
-                      >
-                        Sobre
-                      </a>
-                      <hr />
-                      <button
-                        onClick={logout}
-                        className="w-full text-left px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
-                      >
-                        Sair
-                      </button>
-                    </>
-                  ))
-                : (component = (
-                    <>
-                      <a
-                        href="/login"
-                        className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
-                      >
-                        Login
-                      </a>
-                      <a
-                        href="/cadastro"
-                        className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
-                      >
-                        Criar Conta
-                      </a>
-                    </>
-                  ))}
+              {token ? (
+                <>
+                  <button
+                    onClick={() => setOpen(true)}
+                    className="block w-full text-left px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
+                  >
+                    Meu Perfil
+                  </button>
+                  <ModalPerfil open={open} onClose={() => setOpen(false)}>
+                    <Perfil />
+                  </ModalPerfil>
+                  <a
+                    href="/carrinho"
+                    className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
+                  >
+                    Meu Carrinho
+                  </a>
+                  <a
+                    href="/about"
+                    className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
+                  >
+                    Sobre
+                  </a>
+                  <hr />
+                  <button
+                    onClick={logout}
+                    className="w-full text-left px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]"
+                  >
+                    Sair
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Link to="/login"className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]">
+                    Login
+                  </Link>
+
+                   <Link to="/login"className="block px-4 py-2 hover:bg-[#ff3c00] hover:text-[#fff6cc]">
+                    Criar Conta
+                  </Link>
+                </>
+              )}
             </div>
           )}
         </div>
